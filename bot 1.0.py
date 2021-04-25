4be518faae9c756ab502d92b5473f8076dcbee6e
kostya <kostmar2005@gmail.com>
2021-04-25 14:41:45 +0400

    ep

--- bot 1.0.py
+++ bot 1.0.py
@@ -1,14 +1,12 @@
 import logging
-from telegram.ext import Updater, MessageHandler, Filters
-from telegram.ext import CallbackContext, CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, ConversationHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 import settings
+import functions
 
 
-token = [
-    settings.API_KEY
-]
-TOKEN = token[0]
+TOKEN = settings.API_KEY
 
 PROXY = {
     'proxy_url': settings.PROXY_URL,
@@ -18,21 +16,205 @@ PROXY = {
     }
 }
 
+commands = [
+    "/start - начать диалог с колоботом",
+    "/help - подсказка о том, как начать пользоваться ботом",
+    "/count_sistem - функция для того, что переводить числа из одной системы счисления в другую",
+    "/sequences_a - фунукция для того, чтобы расчитать сумму n первых членов арифметической прогрессии",
+    "/sequences_g - функция для того, чтобы расчитать сумму n первых членов геометрической прогрессии",
+    ""
+]
+
+for_cnt_sis = {
+    "num": 0,
+    "sis1": 0,
+    "sis2": 0
+}
+
+for_seq = {
+    "a1/b1": 0,
+    "d/q": 0,
+    "n": 0,
+    "f": 0
+}
+
+reply_keyboard = [['/address', '/phone'],
+                  ['/site', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
 logging.basicConfig(filename="bot.log", level=logging.INFO)
+
+
+def cnt_sis(update, context):
+    update.message.reply_text("Я умею переводить целые положительные числа из одной системы счисления в другую легко "
+                              "и просто! (я могу перевести любое целое положительное число в (2ой - 10ой) "
+                              "системе счисления в любую другую (так же от 2ой до 10ой))")
+    update.message.reply_text("Вам нужная моя помощь в этом?")
+    if ("да" in update.message.text.lower() or "yes" in update.message.text.lower() or
+        "of course" in update.message.text.lower() or "конечно" in update.message.text.lower()) \
+            and ("не" not in update.message.text.lower() or "no" not in update.message.text.lower()):
+        update.message.reply_text("Введите целое положительное число, которое хотите перевести:")
+        return 1
+    else:
+        update.message.reply_text("Ну ладно, как пожелаете...")
+        return ConversationHandler.END
+
+
+def cnt_sis1(update, context):
+    if not update.message.text.isdigit():
+        update.message.reply_text("Введите, пожалйста, в правильном формате: любое целое положительное число")
+        return 1
+    if int(update.message.text) != float(update.message.text):
+        update.message.reply_text("Введите, пожалйста, в правильном формате: любое целое положительное число")
+        return 1
+    if int(update.message.text) < 0:
+        update.message.reply_text("Введите, пожалйста, в правильном формате: любое целое положительное число")
+        return 1
+    for_cnt_sis["num"] = int(update.message.text)
+    update.message.reply_text("Теперь введите в какой системе считсления оно находится(целое число от 2 до 10)")
+    return 2
+
+
+def cnt_sis2(update, context):
+    if not update.message.text.isdigit():
+        update.message.reply_text("Введите, пожалйста, систему счисления в правильном формате: целое число от 2 до 10")
+        return 2
+    if int(update.message.text) != float(update.message.text):
+        update.message.reply_text("Введите, пожалйста, систему счисления в правильном формате: целое число от 2 до 10")
+        return 2
+    if not (1 < int(update.message.text) < 11):
+        update.message.reply_text("Введите, пожалйста, систему счисления в правильном формате: целое число от 2 до 10")
+        return 2
+    for_cnt_sis["sis1"] = int(update.message.text)
+    update.message.reply_text("А теперь введите в какую систему счисления вы хотите его перевести")
+    return 3
+
+
+def cnt_sis3(update, context):
+    if not update.message.text.isdigit():
+        update.message.reply_text("Введите, пожалйста систему счисления в правильном формате: целое число от 2 до 10")
+        return 3
+    if int(update.message.text) != float(update.message.text):
+        update.message.reply_text("Введите, пожалйста систему счисления в правильном формате: целое число от 2 до 10")
+        return 3
+    if not (1 < int(update.message.text) < 11):
+        update.message.reply_text("Введите, пожалйста систему счисления в правильном формате: целое число от 2 до 10")
+        return 3
+    for_cnt_sis["sis2"] = int(update.message.text)
+    update.message.reply_text(str(for_cnt_sis["num"]) + " в " + str(for_cnt_sis["sis1"]) +
+                              "ой системе счисления равно " + functions.perevod(for_cnt_sis) + " в " +
+                              str(for_cnt_sis["sis2"]) + "ой системе счисления")
+    return ConversationHandler.END
+
+
+def seq_a(update, context):
+    update.message.reply_text("Могу посчитать сумму первых n членов арифметической прогрессии по её первому члену и"
+                              " разности прогрессии! Вам нужна моя помощь?")
+    if ("да" in update.message.text.lower() or "yes" in update.message.text.lower() or
+        "of course" in update.message.text.lower() or "конечно" in update.message.text.lower()) \
+            and ("не" not in update.message.text.lower() or "no" not in update.message.text.lower()):
+        update.message.reply_text("Хорошо, тогда введите первый член вашей арифметической прогрессии(число)")
+        for_seq["f"] = 1
+        return 1
+    else:
+        update.message.reply_text("Ну ладно, как пожелаете...")
+        return ConversationHandler.END
+
+
+def seq_g(update, context):
+    update.message.reply_text("Могу посчитать сумму первых n членов геометрической прогрессии по её первому члену и"
+                              " знаменателю прогрессии! Вам нужна моя помощь?")
+    if ("да" in update.message.text.lower() or "yes" in update.message.text.lower() or
+        "of course" in update.message.text.lower() or "конечно" in update.message.text.lower()) \
+            and ("не" not in update.message.text.lower() or "no" not in update.message.text.lower()):
+        update.message.reply_text("Хорошо, тогда введите первый член вашей геометрической прогрессии(число)")
+        for_seq["f"] = 0
+        return 1
+    else:
+        update.message.reply_text("Ну ладно, как пожелаете...")
+        return ConversationHandler.END
+
+
+def seq1(update, context):
+    if not update.message.text.isdigit():
+        update.message.reply_text("Пожалуйста, введите первый член в правильном формате: число")
+        return 1
+    if for_seq["f"]:
+        update.message.reply_text("А теперь введите разность вашей арифметической прогрессии(число)")
+    else:
+        update.message.reply_text("А теперь введите знаменатель вашей геометрической прогрессии(число)")
+    return 2
+
+
+def seq2(update, context):
+    if not update.message.text.isdigit():
+        if for_seq["f"]:
+            update.message.reply_text("Пожалуйста, введите разность вашей арифметической прогрессии "
+                                      "в правильном формате: число")
+        else:
+            update.message.reply_text("Пожалуйста, введите знаменатель вашей геометрической прогрессии "
+                                      "в правильном формате: число")
+        return 2
+    if
+
+
+
+def address(update, context):
+    update.message.reply_text(
+        "Адрес: г. Москва, ул. Льва Толстого, 16")
+
+
+def phone(update, context):
+    update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+def site(update, context):
+    update.message.reply_text(
+        "Сайт: http://www.yandex.ru/company")
+
+
+def work_time(update, context):
+    update.message.reply_text("График работы: \n"
+                              "пн: 8:00 - 18:00 \n"
+                              "вт: 9:00 - 19:00 \n"
+                              "ср: 8:30 - 18:30 \n"
+                              "чт: 9:30 - 19:30 \n"
+                              "пт: 9:00 - 18:00 \n"
+                              "сб-вс: выходные дни")
+
+
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
+def cmd(update, context):
+    cmds = commands[0]
+    for x in commands[1:]:
+        cmds += "\n"
+        cmds += x
+    update.message.reply_text(cmds)
+
+
+def stop(update, context):
+    update.message.reply_text("ОК")
+    return ConversationHandler.END
+
+
 def start(update, context):
     update.message.reply_text(
-        "Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
+        "Привет! Я колобот. Я не малое умею")
 
 
-def hp(update, context):
+def help(update, context):
     update.message.reply_text(
-        "Я пока не умею помогать... Я только ваше эхо.")
+        "Если вам интересно узнать мои способности и функции, то напишите /commands, чтобы увидеть список моих команд")
 
 
 def main():
@@ -41,9 +223,48 @@ def main():
 
     dp = updater.dispatcher
 
+    count_sis = ConversationHandler(
+        entry_points=[CommandHandler('count_sistem', cnt_sis)],
+
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(Filters.text, cnt_sis1, pass_user_data=True)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(Filters.text, cnt_sis2, pass_user_data=True)],
+
+            3: [MessageHandler(Filters.text, cnt_sis3, pass_user_data=True)]
+        },
+
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    sequences = ConversationHandler(
+        entry_points=[CommandHandler('sequences_a', seq_a), CommandHandler('sequences_g', seq_g)],
+
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(Filters.text, seq1, pass_user_data=True)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(Filters.text, seq2, pass_user_data=True)],
+
+            3: [MessageHandler(Filters.text, seq3, pass_user_data=True)]
+        },
+
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    dp.add_handler(count_sis)
+
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", hp))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("commands", cmd))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler('stop', stop))
+    dp.add_handler(CommandHandler("address", address))
+    dp.add_handler(CommandHandler("phone", phone))
+    dp.add_handler(CommandHandler("site", site))
+    dp.add_handler(CommandHandler("work_time", work_time))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
 
---
+++ functions.py
@@ -0,0 +1,30 @@
+def perevod(a):
+    n = a["num"]
+    s1 = a["sis1"]
+    s2 = a["sis2"]
+    if s1 != 10:
+        ch = 0
+        for i in range(len(str(n))):
+            ch += int(str(n)[i]) * (s1 ** (len(str(n)) - i - 1))
+        n = ch
+        s1 = 10
+    ot = ""
+    while n // s2 != 0:
+        ot = str(n % s2) + ot
+        n //= s2
+    ot = str(n % s2) + ot
+    return ot
+
+
+def sequences(a):
+    if a["f"] == "a":
+        a1 = a["a1/b1"]
+        d = a["d/q"]
+        n = a["n"]
+        s = (2 * a1 + (n - 1) * d) * n / 2
+    else:
+        b1 = a["a1/b1"]
+        q = a["d/q"]
+        n = a["n"]
+        s = b1 * ((q ** n) - 1) / (q - 1)
+    return s
